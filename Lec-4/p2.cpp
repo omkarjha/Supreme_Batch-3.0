9e3e648b93916bd5d626a7bf33abb52b317296df
omkarjha <jhaomkar396@gmail.com>
2024-12-08 08:46:34 +0530

    first comm

---
+++ Lec-4/p2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;  //Variable declariation
+    cout << "Enter the value of 'a' :" << endl;
+    cin >> a;
+    cout << "The value of 'a' is: " << a;
+}
---
+++ Lec-4/p3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//various type of datatypes
+
+int main(){
+    int a = 14;
+    float b = 1.2;
+    char ch = 'a';
+    bool flag = true;
+
+    cout << a << endl << b << endl << ch << endl << flag <<  endl;
+
+
+}
